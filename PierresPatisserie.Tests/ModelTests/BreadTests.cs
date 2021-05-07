92fceb7b0b90d5c83e186c5d740e19cab8eb5eff
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-05-07 09:46:59 -0700

    add first test, instantiate newOrder

--- PierresPatisserie.Tests/ModelTests/BreadTests.cs
+++ PierresPatisserie.Tests/ModelTests/BreadTests.cs
@@ -1,11 +1,24 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PierresPatisserie.Bread;
+using System;
+using System.Collections.Generic;
 
 namespace PierresPatisserie.Tests
 {
   [TestClass]
   public class BreadTests
   {
-    // Test methods go here
+    // test to verify type of a constructor with two fields for quantity and price (if necessary)
+    [TestMethod]
+    public void Order_BuildOrderWithBreadQuantity_Order()
+    {
+      // Act
+      Order newOrder = new Order();
+
+      // Assert
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
   }
 }
+
+// using int breadOrder to gather order input from the user
--- PierresPatisserie/Models/Bread.cs
+++ PierresPatisserie/Models/Bread.cs
@@ -1,7 +1,10 @@
+using System;
+using System.Collections.Generic;
+
 namespace PierresPatisserie.Bread
 {
   public class Bread
   {
-    // properties, constructors, methods, etc. go here
+    // create a constructor with two fields for quantity and price (if necessary)
   }
 }
