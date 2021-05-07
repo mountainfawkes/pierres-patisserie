cad6725453edd4cf62bce8adbe850c84d0b3c49d
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-05-07 14:30:15 -0700

    calls calculation methods and returns cost to the console

--- PierresPatisserie/Project.cs
+++ PierresPatisserie/Project.cs
@@ -9,29 +9,41 @@ namespace PierresPatisserie
   {
     public static void Main()
     {
+      // Set the prices for the day
+      int breadPrice = 5;
+      int pastrySinglePrice = 2;
+      int pastryTrioPrice = 5;
+
       // Display the menu to the console
 
-      Console.WriteLine("BIENVENUE CHEZ PATISSERIE DE PIERRE");
-      Console.WriteLine("-----------------------------------");
-      Console.WriteLine("Today's prices:");
-      Console.WriteLine("BREAD: $5 each loaf");
-      Console.WriteLine("Special: Buy 2, get 1 free");
-      Console.WriteLine("-----------------------------------");
-      Console.WriteLine("PASTRIES: today's pastry is an almond croissant");
-      Console.WriteLine("$2 each pastry");
-      Console.WriteLine("Special: Buy 3 for $5");
-      Console.WriteLine("-----------------------------------");
+      Console.WriteLine("");
+      Console.WriteLine("        BIENVENUE CHEZ PIERRE");
+      Console.WriteLine("");
+      Console.WriteLine("-------------Today's Menu------------");
+      Console.WriteLine("        BREAD: $5 each loaf");
+      Console.WriteLine("     Special: Buy 2, get 1 free");
+      Console.WriteLine("------------------o------------------");
+      Console.WriteLine("       PASTRIES: $2 each pastry");
+      Console.WriteLine("        Special: Buy 3 for $5");
+      Console.WriteLine("Today's pastry is an almond croissant");
+      Console.WriteLine("-------------------------------------");
 
       // Prompt the user for their order
 
+      Console.WriteLine("");
       Console.WriteLine("How many loaves of bread would you like?");
-      int breadOrder = int.Parse(Console.ReadLine());
+      int breadQuantity = int.Parse(Console.ReadLine());
       Console.WriteLine("How many pastries would you like?");
-      int pastryOrder = int.Parse(Console.ReadLine());
+      int pastryQuantity = int.Parse(Console.ReadLine());
 
       // Call methods to calculate the price
+      BreadOrder newBreadOrder = new BreadOrder(breadQuantity, breadPrice);
+      PastryOrder newPastryOrder = new PastryOrder(pastryQuantity, pastrySinglePrice, pastryTrioPrice);
+      int totalCost = newBreadOrder.CalculateBreadCost(breadQuantity, breadPrice) + newPastryOrder.CalculatePastryCost(pastryQuantity, pastrySinglePrice, pastryTrioPrice);
 
       // Return the price to the console
+      Console.WriteLine("Thank you for your order!");
+      Console.WriteLine("Your total cost is $" + totalCost);
     }
   }
 }
