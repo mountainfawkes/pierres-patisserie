b7c6124fc4f0a1819fef84e7843500160c99607b
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-05-07 11:51:48 -0700

    add fifth test, pass red phase, CalculateBreadCost if remainder

--- PierresPatisserie.Tests/ModelTests/BreadTests.cs
+++ PierresPatisserie.Tests/ModelTests/BreadTests.cs
@@ -57,7 +57,21 @@ namespace PierresPatisserie.Tests
       Assert.AreEqual(expectedBreadCost, breadCost);
     }
 
-    
+    [TestMethod]
+    public void CalculateBreadCost_CalculateCostIfRemainder_BreadCost()
+    {
+      // Arrange
+      int breadQuantity = 5;
+      int breadPrice = 5;
+      BreadOrder newBreadOrder = new BreadOrder(breadQuantity, breadPrice);
+
+      // Act
+      int expectedBreadCost = 4 * 5;
+      int breadCost = newBreadOrder.CalculateBreadCost(breadQuantity, breadPrice);
+
+      // Assert
+      Assert.AreEqual(expectedBreadCost, breadCost);
+    }
   }
 }
 
